bf470cff6d4e2ddbb6069016253367ffad9534ea
Animated-Eagle <sheddinghorribly@gmail.com>
2020-07-08 00:42:22 -0400

    lzy imgs

--- themes/site-theme/src/js/main.js
+++ themes/site-theme/src/js/main.js
@@ -12,12 +12,12 @@ document.addEventListener("DOMContentLoaded", function() {
   var lazyloadImagesBk;    
 
   if ("IntersectionObserver" in window) {
-    lazyloadImagesBk = document.querySelectorAll(".lazy");
+    lazyloadImagesBk = document.querySelectorAll(".lzy-bk");
     var imageObserver = new IntersectionObserver(function(entries, observer) {
       entries.forEach(function(entry) {
         if (entry.isIntersecting) {
           var image = entry.target;
-          image.classList.remove("lazy");
+          image.classList.remove("lzy-bk");
           imageObserver.unobserve(image);
         }
       });
@@ -26,34 +26,6 @@ document.addEventListener("DOMContentLoaded", function() {
     lazyloadImagesBk.forEach(function(image) {
       imageObserver.observe(image);
     });
-  } else {  
-    var lazyloadThrottleTimeout;
-    lazyloadImagesBk = document.querySelectorAll(".lazy");
-    
-    function lazyload () {
-      if(lazyloadThrottleTimeout) {
-        clearTimeout(lazyloadThrottleTimeout);
-      }    
-
-      lazyloadThrottleTimeout = setTimeout(function() {
-        var scrollTop = window.pageYOffset;
-        lazyloadImagesBk.forEach(function(img) {
-            if(img.offsetTop < (window.innerHeight + scrollTop)) {
-              img.src = img.dataset.src;
-              img.classList.remove('lazy');
-            }
-        });
-        if(lazyloadImagesBk.length == 0) { 
-          document.removeEventListener("scroll", lazyload);
-          window.removeEventListener("resize", lazyload);
-          window.removeEventListener("orientationChange", lazyload);
-        }
-      }, 20);
-    }
-
-    document.addEventListener("scroll", lazyload);
-    window.addEventListener("resize", lazyload);
-    window.addEventListener("orientationChange", lazyload);
   }
 })
 
